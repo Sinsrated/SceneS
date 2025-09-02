c95d8e5a05676c205aea364fbc64549bc3e8673e
Sinsrated <ratedsinner@gmail.com>
2025-09-02 23:35:18 +0300

    metadata update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import HeroCarousel from "./components/homehero";
 export default function Home() {
   return(
     <div className="">
-      <p> Hello world!</p>
+       <HeroCarousel />
     </div>
   );
     
